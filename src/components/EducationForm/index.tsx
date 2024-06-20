5d46709be25c1ee61c9066898aa3e14c9859e239
Rohit Sharma <mrsharma2404@gmail.com>
2024-06-21 02:08:43 +0530

    added education form

---
+++ src/components/EducationForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// styles import
+import styles from './index.module.scss'
+
+const EducationForm = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div>abcd</div>
+    </div>
+  )
+}
+
+export default EducationForm
